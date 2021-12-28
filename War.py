35830993465a7912739f20d461212dad33afed54
IamOppy <randolffretrato@yahoo.com>
2021-12-28 00:22:17 -0800

    second commit

--- War.py
+++ War.py
@@ -15,7 +15,7 @@ CLOCK = pygame.time.Clock()
 FONT_ = pygame.font.Font(os.path.join(
     "Assets/FONT", "FutureMillennium Black.ttf"), 50)
 
-# GROUND LIMIT = 540
+# GROUND LIMIT = 540 FOR player
 
 
 class Player(pygame.sprite.Sprite):
@@ -69,10 +69,12 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=(400, 540))
         self.gravity = 0
         self.walk_steps = 4
+        self.player_Pos = [self.rect.x, self.rect.y]
 
     def player_input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.rect.bottom == 540:
+
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 540:
             self.gravity -= 30
         if keys[pygame.K_d] and self.rect.bottom == 540:
             self.rect.x += self.walk_steps
@@ -103,22 +105,83 @@ class Player(pygame.sprite.Sprite):
             if self.player_index >= len(self.player_idles_list):
                 self.player_index = 0
             self.image = self.player_idles_list[int(self.player_index)]
+        self.player_Pos = [self.rect.x, self.rect.y]
 
     def update(self):
         self.player_input()
         self.apply_gravity()
         self.animate_state()
 
+    def create_bullet(self):
 
-class Bullet(pygame.sprite.Sprite):
+        return Bullet(self.player_Pos[0], self.player_Pos[1])
+
+
+class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        pass
+        Slime_1 = pygame.image.load(os.path.join(
+            "Assets/Monster1Frames", "SlimeF1.png"))
+        Slime_2 = pygame.image.load(os.path.join(
+            "Assets/Monster1Frames", "SlimeF2.png"))
+        Slime_3 = pygame.image.load(os.path.join(
+            "Assets/Monster1Frames", "SlimeF3.png"))
+        Slime_4 = pygame.image.load(os.path.join(
+            "Assets/Monster1Frames", "SlimeF4.png"))
+        Slime_5 = pygame.image.load(os.path.join(
+            "Assets/Monster1Frames", "SlimeF5.png"))
+        Slime_6 = pygame.image.load(os.path.join(
+            "Assets/Monster1Frames", "SlimeF6.png"))
+
+        self.slime_1 = pygame.transform.scale(Slime_1, (200, 300))
+        self.slime_2 = pygame.transform.scale(Slime_2, (200, 300))
+        self.slime_3 = pygame.transform.scale(Slime_3, (200, 300))
+        self.slime_4 = pygame.transform.scale(Slime_4, (200, 300))
+        self.slime_5 = pygame.transform.scale(Slime_5, (200, 300))
+        self.slime_6 = pygame.transform.scale(Slime_6, (200, 300))
+
+        self.slimeFrames = [self.slime_1, self.slime_2,
+                            self.slime_3, self.slime_4, self.slime_5, self.slime_6]
+        self.slime_index = 0
+
+        self.image = self.slimeFrames[self.slime_index]
+        self.rect = self.image.get_rect(bottomright=(1000, 600))
+
+    def animate_state(self):
+        self.slime_index += 0.1
+        if self.slime_index >= len(self.slimeFrames):
+            self.slime_index = 0
+        else:
+            self.image = self.slimeFrames[int(self.slime_index)]
+
+    def update(self):
+        self.animate_state()
+        self.rect.x -= 3
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            print('Destroyed')
+            self.kill()
 
 
-player = pygame.sprite.Group()
-player.add(Player())
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.Surface((50, 10))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect(center=(pos_x, pos_y))
 
+    def update(self):
+        self.rect.x += 5
+
+
+player = Player()
+player_group = pygame.sprite.Group()
+player_group.add(Player())
+
+enemy_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
 
 # SURFACES
 background_surface = pygame.image.load(
@@ -127,16 +190,28 @@ background_surface = pygame.image.load(
 
 def draw_window():
     SCREEN.blit(background_surface, (0, 0))
-    player.draw(SCREEN)
-    player.update()
+    player_group.draw(SCREEN)
+    player_group.update()
 
+    enemy_group.draw(SCREEN)
+    enemy_group.update()
+
+    bullet_group.draw(SCREEN)
+    bullet_group.update()
+
+
+enemy_Spawn = pygame.USEREVENT + 1
+pygame.time.set_timer(enemy_Spawn, 4000)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         if GAME_ACTIVE:
-            pass
+            if event.type == enemy_Spawn:
+                enemy_group.add(Enemy())
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bullet_group.add(player.create_bullet())
         else:
             """Check for input to restart game ex. Reset time, Reset Enemy Position"""
             pass
